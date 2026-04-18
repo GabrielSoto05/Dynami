790cbba6bbd9cef1893d5d7cc53b9047c002cece
Gabriel Soto <gsoto3894@gmail.com>
2026-04-17 20:35:07 -0500

    implemented the backend of the chatbot

--- dynami/src/app/chatbot/page.js
+++ dynami/src/app/chatbot/page.js
@@ -1,11 +1,38 @@
 'use client'
 
 import Background from "@/app/Components/Background"
+import { useState } from 'react'
 export default function Chatbot() {
+
+    const [currentState, setCurrentState] = useState('GREET')
+
+    const [answers, setAnswers] = useState({
+      age: null,
+      weight: null,
+      goals: null
+    })
+
+    function handleTransition(input, value){
+      setAnswers(prev => ({ ...prev, [input]: value}))
+    
+
+    const transitions = {
+      GREET: { start: 'ASK_AGE'},
+      ASK_AGE: { 'Under 18': 'ASK_WEIGHT', '18-44': 'ASK_WEIGHT', '45+': 'ASK_WEIGHT'},
+      ASK_WEIGHT: {'Under 100lbs': 'ASK_GOAL', '100-150': 'ASK_GOAL','150-175': 'ASK_GOAL','175-200': 'ASK_GOAL', '200+': 'ASK_GOAL'},
+      ASK_GOAL: {'Lose BF(BodyFat)/Gain Muscle': 'RECOMMEND','Gain Muscle/Strength/Maintain BF(BodyFat)': 'RECOMMEND','Maintain Muscle/Gain Strength/Lose BF(Bodyfat)': 'RECOMMEND'},
+      RECOMMEND: { restart: 'GREET'}
+    }
+
+    const nextState = transitions[currentState][input]
+    if(nextState) setCurrentState(nextState)
+    }
+
   return (
     <div className="bg-black min-h-screen text-white flex items-center justify-center">
       <Background/>
-      <h1>Chatbot page</h1>
+
+      
     </div>
   )
 }
